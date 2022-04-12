fe483d159be30f05815ebe6e7c40634bf85545f7
Dmytro <deepdows@gmail.com>
2022-04-12 10:39:25 +0300

    string to camelcase

--- src/app/v1/endpoints/metrics.py
+++ src/app/v1/endpoints/metrics.py
@@ -1,4 +1,5 @@
 import json
+from re import sub
 import pandas as pd
 from fastapi import status, APIRouter
 
@@ -6,9 +7,19 @@ from db.base import engine
 
 router = APIRouter(prefix="/metrics")
 
+def string_to_camelcase(text):
+    text = sub(r"(_|-)+", " ", text).title().replace(" ", "")
+    text = text.replace('.', '')
+    return ''.join([text[0].lower(), text[1:]])
+
 @router.get('/', status_code=status.HTTP_200_OK)
 async def main():
     kasuria_metrics_daily = pd.read_sql_table('kasuria_metrics_daily', engine, schema='metrics')
+    new_names = {}
+    for column in kasuria_metrics_daily.columns:
+        new_names[column] = string_to_camelcase(column)
+    print(new_names)
+    kasuria_metrics_daily = kasuria_metrics_daily.rename(columns=new_names)
     to_json = kasuria_metrics_daily.to_json(orient="records")
 
     return json.loads(to_json)
