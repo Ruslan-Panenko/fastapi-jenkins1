9e0d695bb2523d7c866c1cd38349f2ed6a5d12a7
dmytro.holovchenko <dmytro.holovchenko@tangramltd.com>
2022-04-19 21:04:22 +0300

    adding cors

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from v1.api import router_v1
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router_v1)
