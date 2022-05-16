742212b9d2e3cb228eb7658964e154ab90406e4a
dmytro <dmytro.holovchenko>
2022-05-16 10:54:50 -0400

    adding sessions

--- src/app/db/base.py
+++ src/app/db/base.py
@@ -1,5 +1,11 @@
+import sqlalchemy
 from core.settings import DB_URL
 from sqlalchemy import create_engine
-
+from sqlalchemy.orm import sessionmaker
 
 engine = create_engine(DB_URL)
+metadata = sqlalchemy.MetaData(engine)
+
+Session = sessionmaker(engine)
+
+# 
